0b59fa9abac2c41084e08fbec96311c29ad9c45b
elgalgo <cristofer50056@gmail.com>
2022-08-07 21:56:23 -0300

    ItemDetail v2

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,6 +6,7 @@ function ItemDetail({description, id, imageUrl, price, stock, title}){
         <>
         <h1>{title}</h1>
         <img className="imgItem" src={imageUrl}/>
+        <p>{description}</p>
         <h2>Precio: {price}</h2>
         <p>Stock disponible: {stock}</p>
         <ItemCount id={id} stock={stock} initial={1} onAdd={(cantidad)=>{alert(`Se agregaron ${cantidad} items a su carrito`)}}/>
